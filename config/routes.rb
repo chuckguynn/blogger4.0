45e0e82d45b18ff7dba214c0deaddbb0091967f8
chuckguynn <chuckguynn@me.com>
2013-11-21 05:57:24 -0600

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Blogger40::Application.routes.draw do
 
   root to: 'articles#index'
+
   resources :articles do
     resources :comments
   end
+
   resources :tags
+
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
